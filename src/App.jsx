826999b60f9e24cec3f287ce14bc1b30f5f3c06f
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2023-05-01 02:27:00 +0530

    Added alert on invalid number

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import { useState ,useRef} from 'react'
-import './App.css'
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const inputRef = useRef(null);
-  function clicked(){
+  function clicked() {
     event.preventDefault();
-    const query = inputRef.current.value; // replace with your search query
-    const searchUrl = `https://wa.me/+91${query}`;
-    window.open(searchUrl, "_blank");
+    if (inputRef.current.value.length == 10) {
+      const query = inputRef.current.value; // replace with your search query
+      const searchUrl = `https://wa.me/+91${query}`;
+      window.open(searchUrl, "_blank");
+    } else {
+      window.alert("Please enter a valid number");
+    }
   }
 
   return (
     <>
       <h1>Enter the Number</h1>
-      <input type="number" className='number-input' pattern="[0-9]*" ref={inputRef} />
+      <input
+        type="number"
+        className="number-input"
+        pattern="[0-9]*"
+        ref={inputRef}
+      />
       <br />
-      <button className='button' onClick={()=>clicked()}>Chat!</button>
+      <button className="button" onClick={() => clicked()}>
+        Chat!
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
